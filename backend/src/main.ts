90a8919140b170d46e00c897e8c92967ded84cad
heweit00 <heweit00@hs-esslingen.de>
2022-05-24 13:08:25 +0200

    try to deploy on heroku

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create(AppModule);
@@ -12,19 +12,17 @@ async function bootstrap() {
 
   // Swagger
   const confg = new DocumentBuilder()
-      .setTitle('Todo App')
-      .setDescription('Todo API description')
-      .setVersion('1.0')
-      .addTag('todos')
-      .build();
+    .setTitle('Todo App')
+    .setDescription('Todo API description')
+    .setVersion('1.0')
+    .addTag('todos')
+    .build();
   const document = SwaggerModule.createDocument(app, confg);
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors();
-  await app.listen(port, () => {
-    console.log('[WEB]', config.get<string>('BASE_URL'));
-  });
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
